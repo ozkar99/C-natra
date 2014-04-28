532a12f1b971d030b6389a75cc683d8fdcb0f3e5
R. Diaz de Leon <leon@elinter.net>
2014-04-28 18:20:17 -0500

    Some grooming in the source

--- src/server.c
+++ src/server.c
@@ -1,36 +1,34 @@
 #ifndef __SERVERH__
 #define __SERVERH__
 /* Moreno Garza Oscar
-*  This is the web-server in an effor to port a small set of the Sinatra framework for ruby into C.
+ * This is the web-server in an effor to port a small set of the Sinatra
+ * framework for ruby into C.
 */
 
 /* Based on http://tinyhack.com/2014/03/12/implementing-a-web-server-in-a-single-printf-call/ */
 #include<unistd.h>
 #include<stdio.h>
 #include<string.h>
-#include<strings.h>
 #include<arpa/inet.h>
 
 #include "utils.h"
 #include "parser.h"
 
-/*This function starts the server, return socket file descriptor*/
+/* This function starts the server, return socket file descriptor */
 int serverStart(char *servip, int port) {
- 
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     int backlog = 5;
 
     /* bind this mofo */
     struct sockaddr_in serv_addr;
-    bzero((char *)&serv_addr, sizeof(serv_addr));
-        serv_addr.sin_family = AF_INET;
-        serv_addr.sin_addr.s_addr = inet_addr(servip);
-        serv_addr.sin_port = htons(port);
+    memset((char *)&serv_addr, '\0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(servip);
+    serv_addr.sin_port = htons(port);
     bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
     listen(sockfd, backlog);
 
-
-    /*lame sinatra suckup*/
+    /*Lame sinatra suckup*/
     char servchar[MAX_CHAR_SIZE];
     sprintf(servchar, "http://%s:%d/", servip, port);
     printf("C-natra has taken the stage...%s\n", servchar);
@@ -55,10 +53,6 @@ void serverPackageHandler(int cfd, char *packet) {
     } else {
         serverSendHTML(cfd, "<html><h1>DEFAULT SHIT</H1>");
     }
-
-    /*parsing tests*/
-    //printf("\n\n\n\nParsing Test in server.c:\n");
-    //printf("Method: %s\nRequestPath: %s\nRequestArgs: %s\nProtocol: %s\n", u.method, u.req.path, u.req.arg, u.protocol);
 }
 
 /* Listen for connection and serve them*/
@@ -77,10 +71,7 @@ void serverLoop(int sockfd) {
     }  
 }
 
-/************************* main ***********************/
-/* main routine */ 
 int main(int argc, char *argv[]) {
-
     int socketfd = serverStart("127.0.0.1", 30001);
     serverLoop(socketfd);
 
