63bc53a8fd73dbb4749987d8fae7df2e14b27c11
R. Díaz de León <leon@elinter.net>
2014-04-27 19:45:32 -0500

    replaced legacy function bzero with memset

--- src/server.c
+++ src/server.c
@@ -8,7 +8,6 @@
 #include<unistd.h>
 #include<stdio.h>
 #include<string.h>
-#include<strings.h>
 #include<arpa/inet.h>
 
 #include "utils.h"
@@ -22,10 +21,10 @@ int serverStart(char *servip, int port) {
 
     /* bind this mofo */
     struct sockaddr_in serv_addr;
-    bzero((char *)&serv_addr, sizeof(serv_addr));
-        serv_addr.sin_family = AF_INET;
-        serv_addr.sin_addr.s_addr = inet_addr(servip);
-        serv_addr.sin_port = htons(port);
+    memset((char *)&serv_addr, '\0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(servip);
+    serv_addr.sin_port = htons(port);
     bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
     listen(sockfd, backlog);
 
